e472a8ca8632947edc0c59f66336a46fcb8385d2
Cris <kristiano.gon@gmail.com>
2025-06-02 09:48:13 -0300

    chore: setup sales remote mfe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import SalesProvider from "./components/SalesComponent";
 
-const App = () => {
+function App() {
   return (
-    <div className="content">
-      <SalesProvider />
+    <div className="container">
+      <div className="icon-container">
+        <h2>📊</h2>
+      </div>
+      <h1 className="title">Fiap Farm Sales</h1>
     </div>
   );
-};
+}
 
 export default App;
